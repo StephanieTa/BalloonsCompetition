13a3d341ee9164fcbd227cc96448ecf401c99426
Stephanie Ta <stephanie.ta@outlook.com>
2014-01-03 16:55:02 +0100

    Create class and reorder

---
+++ Balloons/AirTubeView.h
@@ -0,0 +1,13 @@
+//
+//  AirTubeView.h
+//  Balloons
+//
+//  Created by Stephanie Ta on 03.01.14.
+//  Copyright (c) 2014 Stephanie Ta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AirTubeView : UIView
+
+@end
---
+++ Balloons/AirTubeView.m
@@ -0,0 +1,31 @@
+//
+//  AirTubeView.m
+//  Balloons
+//
+//  Created by Stephanie Ta on 03.01.14.
+//  Copyright (c) 2014 Stephanie Ta. All rights reserved.
+//
+
+#import "AirTubeView.h"
+
+@implementation AirTubeView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
